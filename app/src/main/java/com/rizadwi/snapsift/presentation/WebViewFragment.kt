cdacca86b2678174364f18eff45a85307cc1ae45
rizadwiandhika <rizadwiandhika@gmail.com>
2024-02-06 15:12:38 +0700

    refactor: improve ux by show tap effect & loading for webview

--- app/src/main/java/com/rizadwi/snapsift/presentation/WebViewFragment.kt
+++ app/src/main/java/com/rizadwi/snapsift/presentation/WebViewFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.widget.Toast
 import com.rizadwi.snapsift.common.base.BaseFragment
 import com.rizadwi.snapsift.databinding.FragmentWebviewBinding
@@ -24,7 +26,33 @@ class WebViewFragment : BaseFragment<FragmentWebviewBinding>() {
             return
         }
 
+        binding.progressBar.isIndeterminate = false
+        binding.webView.webChromeClient = getWebChromeClient()
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.loadUrl(url)
+    }
+
+    private fun getWebChromeClient(): WebChromeClient {
+        return object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+
+                if (newProgress == 100) {
+                    binding.webView.visibility = View.VISIBLE
+                    binding.progressBar.visibility = View.GONE
+                } else {
+                    binding.progressBar.setProgress(newProgress, true)
+
+                    binding.webView.visibility = View.GONE
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.webView.webChromeClient = null
     }
 }
