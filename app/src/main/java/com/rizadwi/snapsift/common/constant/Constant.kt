51a889418d0ef5ba959e52125b1cfb6dd94bfb47
rizadwiandhika <rizadwiandhika@gmail.com>
2024-02-06 21:52:02 +0700

    feat: show url and title bar for webview

--- app/src/main/java/com/rizadwi/snapsift/common/constant/Constant.kt
+++ app/src/main/java/com/rizadwi/snapsift/common/constant/Constant.kt
@@ -4,8 +4,8 @@ class Constant {
     companion object {
         const val NEWS_API_ENDPOINT = "https://newsapi.org/v2/"
 
-        //        const val NEWS_API_KEY = "4888af1168554a48abf1e2a075e8ef00"
-        const val NEWS_API_KEY = "ffee6e40f6b04efba4a74eb6f3215c3b"
+        const val NEWS_API_KEY = "4888af1168554a48abf1e2a075e8ef00"
+//        const val NEWS_API_KEY = "ffee6e40f6b04efba4a74eb6f3215c3b"
 
         const val NEWS_API_KEY_HEADER = "X-Api-Key"
 
