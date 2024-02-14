d9967f39c7a142e78867a8b679220a85359ffe4f
rizadwiandhika <rizadwiandhika@gmail.com>
2024-02-14 23:45:03 +0700

    refactor: remove onexit animation

--- app/src/main/java/com/rizadwi/snapsift/common/constant/Constant.kt
+++ app/src/main/java/com/rizadwi/snapsift/common/constant/Constant.kt
@@ -4,9 +4,9 @@ class Constant {
     companion object {
         const val NEWS_API_ENDPOINT = "https://newsapi.org/v2/"
 
-        // const val NEWS_API_KEY = "4888af1168554a48abf1e2a075e8ef00" // Riza
+        const val NEWS_API_KEY = "4888af1168554a48abf1e2a075e8ef00" // Riza
         // const val NEWS_API_KEY = "ffee6e40f6b04efba4a74eb6f3215c3b" // Tiara
-        const val NEWS_API_KEY = "76bcb31b5d794201837625d2489f517b" // Enzo Iqbal
+        // const val NEWS_API_KEY = "76bcb31b5d794201837625d2489f517b" // Enzo Iqbal
 
         const val NEWS_API_KEY_HEADER = "X-Api-Key"
 
